ace6082718a3e734b3f687d443b70d6c4755d414
[abrahamgiron7] <[abrahamgiron7@gmail.com]>
2023-09-22 01:18:45 +0200

    dfhg

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function nuevoContenido() {
-/*     document.open();
-    document.write(
-      "<h1>Quita el contenido viejo - Agrega el contenido nuevo!</h1>",
-    );
-    document.close(); */
-  }
+var sign = prompt("What's your sign?");
+console.log(sign);
+if (sign.toLowerCase() == "scorpio") {
+  alert("Wow! I'm a Scorpio too!");
+}
+
+/* var myCar = new Object();
+myCar.make = "sss";
+myCar.model = "Mustang";
+myCar.year = 1969;
+console.log(myCar.make); */
